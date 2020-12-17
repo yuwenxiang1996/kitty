1cc8a6b6c81f98366015e69604ed7d38c01e9412
wx_yu <wenxiang_yu2333@163.com>
2020-12-17 03:37:28 +0100

    unateness_changed

--- include/kitty/threshold_identification.hpp
+++ include/kitty/threshold_identification.hpp
@@ -20,9 +20,12 @@ bool is_threshold( const TT& tt, std::vector<int64_t>* plf = nullptr )
   // Get some paramters
   int32_t num_bits = static_cast<uint32_t>( tt.num_bits() );
   int32_t num_vars = static_cast<uint32_t>( tt.num_vars() );
-  auto tt_flip = tt;
-  //int flip_table[40];
-  int i, j, k, v, wk;
+  
+  auto ttf = tt;
+  int flip_table[50];
+  int i, j, m, v, wk;
+    
+    
   // Print truth table
   /*std::cout << "Truth table of f is:";
   for ( int32_t i = num_bits - 1; i >= 0; i-- )
@@ -31,62 +34,66 @@ bool is_threshold( const TT& tt, std::vector<int64_t>* plf = nullptr )
   }
   std::cout << endl;
  */
-// check unateness, create positive unate function
+ 
+    //check unateness, create positive unate function
     for (auto i = 0u; i < num_vars; i++)
     {
-        auto const tt_1 = cofactor0( tt_flip, i);
-        auto const tt_2 = cofactor1( tt_flip, i);
+        auto const tt_1 = cofactor0( ttf, i);
+        auto const tt_2 = cofactor1( ttf, i);
         auto const s_t = tt_1 | tt_2;
         
-        if (tt_1 = tt_2)
+        if ( implies( tt_2 , tt_1 ) )  // if positive unate
         {
-            continue;
+            ttf = flip ( ttf, i);
+            flip_table[i] = 1;
         }
-        else if (tt_1 == s_t)
+        else if ( implies (tt_1 , tt_2 ) ) // if negative unate
         {
-            flip_inplace( tt_flip, i);
-            neg_unate.push_back( i );
+            flip_table[i] = 0;
         }
-        else if ( tt_2 != s_t)
+        else if (tt_2 != s_t)         // if binate
         {
+            /* binate tt is non-TF*/
             return false;
         }
     }
+ 
   // Get cofactor
-  //for ( i = 0u; i < num_vars ; ++i )
-  //{
-  //  auto fxi     = cofactor1(tt, i);
-  //  auto not_fxi = cofactor0(tt, i);
+  /*for ( i = 0u; i < num_vars ; ++i )
+  {
+    auto fxi     = cofactor1(tt, i);
+    auto not_fxi = cofactor0(tt, i);
   
     // Judge f is positive or negative unate
-  //   if ( implies( fxi, not_fxi ) )
-  //  {
-  //    std::cout << "f is negative unate in x" << i << endl;
-  //    tt_flip = flip( tt_flip, i ); // Flip xi so f_flip is positive unate in xi
-  //   flip_table[i] = 1;
-  //  }
-  //  else if ( implies( not_fxi, fxi ) )
-  //  {
-  //    std::cout << "f is positive unate in x" << i << endl;
-  //    flip_table[i] = 0;
-   // }
-  //  else
-  //  {
-  //    std::cout << "f is binate in x" << i << endl;
-  //    return false;
-  //  }
- // }
+     if ( implies( fxi, not_fxi ) )
+    {
+      std::cout << "f is negative unate in x" << i << endl;
+      tt_flip = flip( tt_flip, i ); // Flip xi so f_flip is positive unate in xi
+     flip_table[i] = 1;
+    }
+    else if ( implies( not_fxi, fxi ) )
+    {
+      std::cout << "f is positive unate in x" << i << endl;
+      flip_table[i] = 0;
+    }
+    else
+    {
+      std::cout << "f is binate in x" << i << endl;
+      return false;
+    }
+  }
   // check tt_flip
-    
-  // Print truth table of f_flip
-  //std::cout << "Truth table of f_flip is:";
-  //for (  i = num_bits - 1; i >= 0; i-- )
-  //{
-  //  std::cout << get_bit( tt_flip, i );
-  //}
-  //std::cout << endl;
+  */
+   //Print truth table of f_flip
+  /*std::cout << "Truth table of f_flip is:";
+  for (  i = num_bits - 1; i >= 0; i-- )
+  {
+    std::cout << get_bit( tt_flip, i );
+  }
+  std::cout << endl;
+   
 
-  /* Create a New ILP problem with tt_flip*/
+  /* Create a New ILP problem with tt_flip */
   lprec* lp;
   int Ncol, *colno = NULL, ret = 0;
   REAL* row = NULL;
@@ -97,9 +104,9 @@ bool is_threshold( const TT& tt, std::vector<int64_t>* plf = nullptr )
   // Set var names
   if ( ret == 0 )
   {
-    for ( k = 0; k < num_vars; k++ )
+    for ( m = 0; m < num_vars; m++ )
     {
-      set_col_name( lp, k+1, "C1" );
+      set_col_name( lp, m+1, "C1" );
     }
     set_col_name( lp, num_vars + 1, "T" );
 
@@ -107,24 +114,24 @@ bool is_threshold( const TT& tt, std::vector<int64_t>* plf = nullptr )
     colno = (int*)malloc( Ncol * sizeof( *colno ) );
     row = (REAL*)malloc( Ncol * sizeof( *row ) );
     if ( ( colno == NULL ) || ( row == NULL ) )
-      ret = 2;
+      ret = 1;
   }
   for ( j = 0; j < num_bits; j++ )
   {
     v = j;
-    for ( k = 0; k < num_vars; k++ )
+    for ( m = 0; m < num_vars; m++ )
     {
-      colno[k] = k + 1; /* k column */
+      colno[m] = m + 1; /* m column */
       
       wk = v % 2;
       v = v >> 1;
       //std::cout << wk;
-      row[k] = wk? 1 : 0;
+      row[m] = wk? 1 : 0;
     }
     //std::cout << endl;
-    col[num_vars] = num_vars + 1; /* num_vars column */
+    colno[num_vars] = num_vars + 1; /* num_vars column */
     row[num_vars] = -1;
-    if ( get_bit(tt_flip,j) )
+    if ( get_bit(ttf,j) )
         add_constraintex( lp, num_vars + 1, row, colno, GE, 0 );
     else
         add_constraintex( lp, num_vars + 1, row, colno, LE, -1 );
@@ -134,46 +141,43 @@ bool is_threshold( const TT& tt, std::vector<int64_t>* plf = nullptr )
   {
     set_add_rowmode( lp, FALSE ); /* rowmode should be turned off again when finished building the model */
 
-    for ( k = 0; k <= num_vars; k++ )
+    for ( m = 0; m <= num_vars; m++ )
     {
-      colno[k] = k + 1; /* k column */
-      row[k] = 1;
+      colno[m] = m + 1; /* k column */
+      row[m] = 1;
     }
     /* set the objective in lpsolve */
     if ( !set_obj_fnex( lp, num_vars + 1, row, colno ) )
-      ret = 4;
+      ret = 2;
   }
 
   // Set variables to int
   if ( ret == 0 )
   {
-    for ( k = 1; k <= num_vars + 1; k++ )
-      set_int( lp, k, TRUE );
+    for ( m = 1; m <= num_vars + 1; m++ )
+      set_int( lp, m, TRUE );
   }
 
   if ( ret == 0 )
   {
-    /* set the object direction to minimize */
     set_minim( lp );
 
-    /* just out of curioucity, now show the model in lp format on screen */
     write_LP( lp, stdout );
 
-    /* I only want to see important messages on screen while solving */
     set_verbose( lp, IMPORTANT );
 
-    /* Now let lpsolve calculate a solution */
+    /*  lpsolve calculate a solution */
     ret = solve( lp );
     if ( ret == OPTIMAL )
       ret = 0;
     else
-      ret = 5;
+      ret = 3;
   }
 
   if ( ret == 0 )
   {
     /* objective value */
-    printf( "Objective value: %f\n", get_objective( lp ) );
+    //printf( "Objective value: %f\n", get_objective( lp ) );
 
     /* variable values */
     get_variables( lp, row );
@@ -197,18 +201,18 @@ bool is_threshold( const TT& tt, std::vector<int64_t>* plf = nullptr )
     linear_form.push_back( T );
   }
 
-  if ( row != NULL )
-    free( row );
-  if ( col != NULL )
-    free( colno );
-
-  /* clean up such that all used memory by lpsolve is free */
-  if ( lp != NULL )
-    delete_lp( lp );
-
-  /* if tt is non-TF: */
+ /* if tt is non-TF */
   if (ret != 0 )
+  {
+    if ( row != NULL )
+     free( row );
+    if ( colno != NULL )
+     free( colno );
+    if ( lp != NULL )
+        delete_lp( lp);
     return false;
+  }
+    
 
   /* if tt is TF: */
   /* push the weight and threshold values into `linear_form` */
